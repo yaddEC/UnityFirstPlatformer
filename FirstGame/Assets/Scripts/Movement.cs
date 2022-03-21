3a1d376ab6199325a23c20c7cb40d586fa1cad2c
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2022-03-21 16:04:24 +0100

    First commit

---
+++ FirstGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+    private CapsuleCollider CapsuleCollider;
+    public LayerMask groundLayer;
+ 
+    public float GroundDistance = 5.1f;
+    public float JumpHeight = 2f;
+    [Range(0, 100f)] [SerializeField] private float speed = 0f;
+    private float gravity;
+
+
+    float horizontal = 0f;
+    
+    public bool isGrounded ;
+    float radius;
+
+    public Vector3 pos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        CapsuleCollider = GetComponent<CapsuleCollider>();
+    
+    }
+
+
+    void Update()
+    {
+        
+        pos = transform.position + Vector3.up * CapsuleCollider.radius;
+        isGrounded = Physics.CheckSphere(pos, CapsuleCollider.radius, groundLayer);
+        horizontal = Input.GetAxisRaw("Horizontal") * speed; 
+
+        if (isGrounded && gravity < 0)
+            gravity = 0f;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            gravity += Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y);
+
+        gravity += Physics.gravity.y * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        float moveFactor = horizontal * Time.fixedDeltaTime;
+
+
+        rb.velocity = new Vector3(0, gravity, moveFactor * 10f);
+    }
+}
